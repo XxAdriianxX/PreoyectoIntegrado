e8e4ecc1967630bd0bc3f883e43006c4e2a59824
Felipe Santamaría <fesalo@floridauniversitaria.es>
2024-05-13 09:53:57 +0200

    Prueba

---
+++ ProyectoPersonal/home.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Prueba Felipe</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <header>
+            <nav class="navbar navbar-expand-sm navbar-light bg-light">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img src="img/ej3/logo.jpg" style="width: 30px;">
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link disabled" aria-current="page" href="#">Episodio 1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 3</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 4</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Episodio 5
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">¡Esta SÍ!</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 6</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 7</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 8</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Episodio 9</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section>
+            <article>
+            </article>
+        </section>
+        <footer></footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
