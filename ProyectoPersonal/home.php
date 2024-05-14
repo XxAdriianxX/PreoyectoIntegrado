631a08eeba5f3f1b83578f6e7171e1a6569c2ffa
Felipe Santamaría <fesalo@floridauniversitaria.es>
2024-05-14 11:40:19 +0200

    Aside terminado + logo implementado navbar

--- ProyectoPersonal/home.php
+++ ProyectoPersonal/home.php
@@ -32,8 +32,10 @@
             background-color: #F5F5DC;
         }
 
-        aside{
-            background-image: ;
+        aside {
+            height: 100%;
+            width: 100%;
+            background-image: url(Assets/img/fondo.jpg);
         }
     </style>
 </head>
@@ -48,8 +50,8 @@
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <a class="navbar-brand" href="#">
-                        <img src="img/ej3/logo.jpg" style="width: 30px;">
+                    <a class="navbar-brand border border-light rounded-circle bg-light" href="#">
+                        <img src="Assets/img/logop.png" style="width: 60px;">
                     </a>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -57,7 +59,7 @@
                                 <a class="nav-link" aria-current="page" href="#">Inicio</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Apartado 2</a>
+                                <a class="nav-link" href="#">Perfil</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Apartado 3</a>
@@ -83,86 +85,91 @@
                 </div>
             </nav>
         </header>
-        <section>
-            <div class="row">
-                <div class="col-3">
-                    <aside>
-                        <h2 style="color:black">AMIGOS</h2>
-                    </aside>
-                </div>
-                <div class="col-9">
-                    <div class="row">
-                        <h2 class="mb-4 rounded-pill  mx-auto custom-bg" style="width:95%">EVENTOS PRÓXIMOS</h2>
-                        <div class="col-lg-6 col-md-6">
-                            <div class="card custom-bg  text-center mb-5">
-                                <div class="card-body">
-                                    <h5 class="card-title"><strong>RECOGIDA DE BASURA</strong></h5>
-                                    <p class="card-text"><br>Ubicación: Parque natural de
-                                        L'albufera</p>
-                                    <div class="row justify-content-start mb-2">
-                                        <div class="col-md-6">
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Fecha:
-                                                27/03/2024</span>
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Horas:
-                                                3</span>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Hora:
-                                                17:00</span>
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Puntos:
-                                                10</span>
-                                        </div>
-                                    </div>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum porro quod
-                                        veniam nemo eum qui voluptatem, eius inventore deleniti, at voluptate
-                                        repudiandae aspernatur beatae pariatur cumque ab velit aut!</p>
-                                    <a href="#" class="btn custom-button border border-dark text-white">Apuntarse</a>
-                                </div>
-                                <img src="Assets/img/albufera.jpg" class="card-img-bottom" alt="...">
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6">
-                            <div class="card custom-bg  text-center mb-5">
-                                <div class="card-body">
-                                    <h5 class="card-title"><strong>RECOGIDA DE BASURA</strong></h5>
-                                    <p class="card-text"><br>Ubicación: Parque natural de
-                                        L'albufera</p>
-                                    <div class="row justify-content-start mb-2">
-                                        <div class="col-md-6">
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Fecha:
-                                                27/03/2024</span>
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Horas:
-                                                3</span>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Hora:
-                                                17:00</span>
-                                            <span
-                                                class="badge rounded-pill pill-bg border border-dark d-block mb-2">Puntos:
-                                                10</span>
-                                        </div>
-                                    </div>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum porro quod
-                                        veniam nemo eum qui voluptatem, eius inventore deleniti, at voluptate
-                                        repudiandae aspernatur beatae pariatur cumque ab velit aut!</p>
-                                    <a href="#" class="btn custom-button border border-dark text-white">Apuntarse</a>
-                                </div>
-                                <img src="Assets/img/albufera.jpg" class="card-img-bottom" alt="...">
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div class="row">
+            <div class="col-2">
+                <aside>
+                    <h2 style="color:black">AMIGOS</h2>
+                </aside>
             </div>
-            <article>
-            </article>
-        </section>
+            <div class="col-10">
+                <section>
+                    <article>
+                        <div class="row">
+                            <h2 class="mb-4 rounded-pill  mx-auto custom-bg" style="width:95%">EVENTOS PRÓXIMOS</h2>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="card custom-bg  text-center mb-5">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><strong>RECOGIDA DE BASURA</strong></h5>
+                                        <p class="card-text"><br>Ubicación: Parque natural de
+                                            L'albufera</p>
+                                        <div class="row justify-content-start mb-2">
+                                            <div class="col-md-6">
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Fecha:
+                                                    27/03/2024</span>
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Horas:
+                                                    3</span>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Hora:
+                                                    17:00</span>
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Puntos:
+                                                    10</span>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum porro
+                                            quod
+                                            veniam nemo eum qui voluptatem, eius inventore deleniti, at voluptate
+                                            repudiandae aspernatur beatae pariatur cumque ab velit aut!</p>
+                                        <a href="#"
+                                            class="btn custom-button border border-dark text-white">Apuntarse</a>
+                                    </div>
+                                    <img src="Assets/img/albufera.jpg" class="card-img-bottom rounded-3" alt="...">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <div class="card custom-bg  text-center mb-5">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><strong>RECOGIDA DE BASURA</strong></h5>
+                                        <p class="card-text"><br>Ubicación: Parque natural de
+                                            L'albufera</p>
+                                        <div class="row justify-content-start mb-2">
+                                            <div class="col-md-6">
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Fecha:
+                                                    27/03/2024</span>
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Horas:
+                                                    3</span>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Hora:
+                                                    17:00</span>
+                                                <span
+                                                    class="badge rounded-pill pill-bg border border-dark d-block mb-2">Puntos:
+                                                    10</span>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum porro
+                                            quod
+                                            veniam nemo eum qui voluptatem, eius inventore deleniti, at voluptate
+                                            repudiandae aspernatur beatae pariatur cumque ab velit aut!</p>
+                                        <a href="#"
+                                            class="btn custom-button border border-dark text-white">Apuntarse</a>
+                                    </div>
+                                    <img src="Assets/img/albufera.jpg" class="card-img-bottom rounded-3" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </section>
+            </div>
+        </div>
+
         <footer></footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
