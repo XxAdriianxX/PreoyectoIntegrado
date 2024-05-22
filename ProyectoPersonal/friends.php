89fd062d7369f328d16b6fe4b009ae5120cbdc57
Felipe Santamaría <fesalo@floridauniversitaria.es>
2024-05-22 20:06:22 +0200

    Card friends problem solved

--- ProyectoPersonal/friends.php
+++ ProyectoPersonal/friends.php
@@ -13,7 +13,7 @@ $security = new Security();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    <link rel="stylesheet" href="Assets/css/events.css">
+    <link rel="stylesheet" href="">
     <title>Prueba Felipe</title>
     <style>
         .custom-bg {
@@ -85,6 +85,8 @@ $security = new Security();
         .card-text {
             color: #343a40;
         }
+
+        
     </style>
 </head>
 
